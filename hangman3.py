3a46d0102a68f76432eb02492adbd2850c22ca8c
ArgyelStoat <23minds@gmail.com>
2013-10-15 21:29:21 -0600

    restart!

--- hangman3.py
+++ hangman3.py
@@ -1,41 +1,38 @@
 f=open('words_list.txt')
 words=f.readlines()
 f.close
-
 from random import choice
-secret=choice(words)
 
 questions3=0
 while questions3<3:
   play=input('Wanna play hangman? y/n: ')
   questions3 +=1
-  if play!='y':
-    print('Haha, I must have misheard you.  I SAID:')
   if play=='y':
     break
+  else:
+    print('Haha, I must have misheard you.  I SAID:')
   if questions3==3:
     print("WRONG.  We're playing now.")
     break
 
-guesslist=[]
-failcount=0
-donecount=0
-blank='*'*(len(secret)-1)
-play_on=1
-alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+while True:
+  secret=choice(words)
+  guesslist=[]
+  failcount=0
+  donecount=0
+  blank='*'*(len(secret)-1)
 
-while play_on==1:
-  print(blank)
-  guess=input("Guess a letter: ")
+  while True:
+    print(blank)
+    print(secret)
+    guess=input("Guess a letter: ").lower()
 
-  if len(guess)!=1:
-    print('Guess one letter dumbass.  And:')
-
-  if guess not in alpha:
-    print('Lower case letters, if you please.')
-
-
-  if guess in alpha:
+    if len(guess)!=1:
+      print('Only one guess at a time.  And:')
+      
+    if not guess.isalpha():
+      print('This needs to be a letter dipwad.')
+      continue
 
     if guess in guesslist and secret:
       print('You already guessed '+guess+'.')
@@ -46,19 +43,21 @@ while play_on==1:
           blank=blank[:i]+secret[i]+blank[i+1:]
           guesslist+=guess
           donecount+=1  
+      if donecount==(len(secret)-1):
+        print('YOU WIN!!!')
+        break
 
-    if guess not in secret:
+    else:
       if guess in guesslist:
         print('you already guessed '+guess+'.')
       else:   
         print('Nope! '+str(6-failcount)+' strikes left.')
         failcount+=1
         guesslist+=guess
-  
-  if donecount==(len(secret)-1):
-    print('YOU WIN!!!')
-    play_on=0 
- 
-  if failcount>6:
-    print('GAME OVER....the secret word was: '+secret+'.')
-    play_on=0
+      if failcount>6:
+        print('GAME OVER....the secret word was: '+secret+'.')
+        break
+
+  play_again=input("Play again? y/n: ")
+  if play_again!='y':
+    break
